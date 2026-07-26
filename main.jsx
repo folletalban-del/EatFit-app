9f8a7d73345bf049a046774920d744c604506a70
folletalban-del <folletalban@gmail.com>
2026-07-26 15:31:43 +0200

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CroquefitApp from './App.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CroquefitApp />
+  </React.StrictMode>
+);
